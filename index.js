5d922437f8e0ef28f06f94e3295a6f7242924e67
Oliver Miller <miller.oliver@icloud.com>
2020-02-18 15:51:00 -0600

    added styling, result page (results.ejs),

--- index.js
+++ index.js
@@ -6,30 +6,40 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// respond with "hello world" when a GET request is made to the homepage
 const baseURL = "https://www.googleapis.com/customsearch/v1?cx=001740279159328743821:7iae50xhvn1&key=AIzaSyA2I-1Dt3W4cHgM4Eb-YQuSUiNlbNYUA6Y";
 app.get('/', async function (req, res) {
-    const q = decodeURI(req.query["q"]);
-    if ("q" in req.query && req.query["q"] !== undefined && req.query["q"] !== " ") await res.json(await showResults(q));
+    const q = req.query["q"];
+    if ("q" in req.query && req.query["q"] !== undefined && req.query["q"] !== " ") await res.render('results.ejs', await showResults(urlencode(q)));
     else res.sendFile("./html/start.html", { root: __dirname });
 });
 
+function urlencode(str) {
+    str = (str + '').toString();
+    str = str.replace('%', '%25');
+
+    // Tilde should be allowed unescaped in future versions of PHP (as reflected below), but if you want to reflect current
+    // PHP behavior, you would need to add ".replace(/~/g, '%7E');" to the following.
+    return decodeURIComponent(str)
+        .replace('%21', '!')
+        .replace('%27', '\'')
+        .replace('%28', '(')
+        .replace('%29', ')')
+        .replace('%2A', '*')
+        .replace('+', ' ');
+}
+
 async function showResults(q) {
     const getJSON = bent('json');
-    let req = await getJSON(baseURL + "&q=" + q);
+    let req = await getJSON(baseURL + "&q=" + encodeURIComponent(q));
+
     let obj = await req;
 
 
-    // THIS IS WHERE I LEFT OFF
-    // let badSpelling = obj.hasOwnProperty("spelling");
-    // if (badSpelling)
-    //
     const searchResult = {
         query: obj["queries"]["request"][0]["searchTerms"],
         numOfResults: obj["queries"]["request"][0]["count"],
         searchTime: obj["searchInformation"]["formattedSearchTime"],
-        //spelling: {}
-
+        results: obj["items"],
     };
     return(searchResult);
 }
